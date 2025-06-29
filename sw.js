a83ca2496f9504b8add8792cbc0849d049470821
hajime-study <hontani.hajime@gmail.com>
2025-06-29 12:16:57 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'hajime-study-cache-v1';
+const urlsToCache = [
+  '/',
+  '/database1900/index.html',
+  '/IMG_1211.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
