2600d2f016c96fe4a5a958e24700e21d90cc59a7
hajime-study <hontani.hajime@gmail.com>
2025-07-03 22:44:11 +0900

    Update sw.js

--- target1900/sw.js
+++ target1900/sw.js
@@ -9,10 +9,14 @@ const urlsToCache = [
   '/target1900/ZenKurenaido-Regular.ttf'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
