844f4632ae0ec1a77fcdb232ea35b18bcc0bfe7a
hajime-study <hontani.hajime@gmail.com>
2025-11-30 21:18:55 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = 'hajime-site-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icon.jpeg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
