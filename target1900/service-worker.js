ee6796439b43cf89838975c93ff9343c5547dad8
hajime-study <hontani.hajime@gmail.com>
2025-11-30 17:34:00 +0900

    Update service-worker.js

--- target1900/service-worker.js
+++ target1900/service-worker.js
@@ -1,21 +1,25 @@
-const cacheName = 'hajime1900-cache-v1';
-const filesToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  '/target1900/target1900.png'
+const cacheName = "hajime1900-v1";
+const assetsToCache = [
+  "/target1900/",
+  "/target1900/index.html",
+  "/target1900/style.css",
+  "/target1900/app.js",
+  "/target1900/words.json",
+  "/target1900/target1900.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
   );
 });
